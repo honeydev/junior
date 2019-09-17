21276e80fb3cc5db38d31bf032a922857292dd90
Igor Shcherbakov <igor.shcherbakov@fogstream.ru>
2019-09-18 09:55:59 +1000

    Базовая структура, модель юзера, view.

--- settings.py
+++ settings.py
@@ -1,5 +1,10 @@
 """Модуль настроек проекта."""
 
 from dotenv import load_dotenv, find_dotenv
-
+import os
 load_dotenv(find_dotenv())
+
+
+DATABASE_URL = os.getenv(
+    'DATABASE_URL', 'postgres://junior:junior@127.0.0.1:5432/junior'
+)
