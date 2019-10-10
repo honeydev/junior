ab61f11eb338829b9b6bddb061bd6a6c279e6c2c
igor shcherbakov <igor.shcherbakov@fogstream.ru>
2019-10-10 18:28:55 +1000

    Base Answer

--- src/qa/views.py
+++ src/qa/views.py
@@ -1,21 +1,44 @@
-from flask import Blueprint, render_template
-from flask.views import MethodView
+from flask import Blueprint, redirect, render_template, request, session
 
-bp = Blueprint('qa', __name__, template_folder='templates')
+from src.qa.forms import AnswerForm
+from src.qa.models import Answer, Question
+from src.views import BaseView
+
+bp: Blueprint = Blueprint('answers', __name__, template_folder='templates')
 
 
-class Answers(MethodView):
-    def __init__(self, template_name):
-        self.template: str = template_name
+class AnswerView(BaseView):
+    def get(self, question_id):
+        question = Question.query.get(question_id)
+        answers = Answer.query.filter_by(
+            question_id=question_id,
+        ).all()
+        self.context.update(
+            dict(
+                auth=session.get('auth'),
+                question=question,
+                answers=answers,
+                form=AnswerForm(),
+            ),
+        )
+        return render_template(self.template_name, **self.context)
 
-    def get(self):
-        return render_template(self.template)
+    def post(self, question_id):
+        form = AnswerForm(request.form)
+        if not form.validate():
+            return render_template(self.template_name, **{'form': form})
+        if form.text.data:
+            answer = Answer(
+                text=form.text.data,
+                question_id=question_id,
+            )
+            Answer.save(answer)
+        return redirect(request.url)
 
 
 bp.add_url_rule(
-    '/qa/',
-    view_func=Answers.as_view(
-        name='qa',
-        template_name='FAQ_page.jinja2',
+    '/answer/<int:question_id>/',
+    view_func=AnswerView.as_view(
+        name='answer', template_name='answer.jinja2',
     ),
 )
