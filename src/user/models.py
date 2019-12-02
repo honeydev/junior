d23b0bdd8531b57a2dddce5265a2e9af5c030353
OlgaSmetyukh <olga.smetiukh@fogstream.ru>
2019-12-02 10:03:11 +1000

    Changed gravatar field.

--- src/user/models.py
+++ src/user/models.py
@@ -1,3 +1,4 @@
+# import enum
 import enum
 import os
 from hashlib import md5
@@ -7,7 +8,6 @@ import jwt
 from flask import current_app as junior_app
 from flask import flash, redirect, session, url_for
 from flask_bcrypt import check_password_hash, generate_password_hash
-from sqlalchemy.dialects.postgresql import ENUM
 
 from src.extensions import db
 
@@ -15,7 +15,7 @@ from src.extensions import db
 # class AvatarType(enum.Enum):
 #     gravatar = 'gravatar'
 #     face = 'face'
-#     # false = False
+# false = False
 
 
 class User(db.Model):  # noqa: WPS230
@@ -59,17 +59,18 @@ class User(db.Model):  # noqa: WPS230
     lastname = db.Column(db.String(), nullable=True)
     image = db.Column(db.String(), default=email, nullable=True)
     # gravatar = db.Column(db.Boolean(), default=True, nullable=True)
-    avatar_types = (
-        'gravatar',
-        'face',
-    )
-    avatar_types_enum = ENUM(*avatar_types, name="avatar_types")
-    gravatar = db.Column(avatar_types_enum, default='gravatar', nullable=True)
-    # gravatar = db.Column(
-    #     db.Enum(AvatarType),
-    #     default=AvatarType.gravatar,
-    #     nullable=True,
+    # avatar_types = (
+    #     'gravatar',
+    #     'face',
     # )
+    # avatar_types_enum = ENUM(*avatar_types, name="avatar_types")
+    # gravatar = db.Column(avatar_types_enum, default='gravatar', nullable=True)
+    gravatar = db.Column(
+        db.Enum('gravatar', 'face'),
+        default='gravatar',
+        name='avatar_type',
+        nullable=True,
+    )
 
     github_id = db.Column(db.String(), nullable=True)
     is_oauth = db.Column(db.Boolean, default=False, nullable=False)
